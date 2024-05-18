a38d8a69de390e7bc1745732d4d0fd5ea17fef64
William Westwood <wwstwd@gmail.com>
2024-05-18 18:32:50 +0100

    Added favicon

--- designed/nuxt.config.ts
+++ designed/nuxt.config.ts
@@ -2,4 +2,15 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/content', 'nuxt-icon'],
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/ww-favicon.svg',
+        },
+      ],
+    },
+  },
 });
